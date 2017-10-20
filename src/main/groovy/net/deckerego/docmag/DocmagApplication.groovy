d8cb69e8e0a95741f56cfffd8b48208202a394a8
John Ellis <deckerego@gmail.com>
2017-10-19 20:47:44 -0400

    Initial bootstrap for Spring Boot 2 + Groovy

---
+++ src/main/groovy/net/deckerego/docmag/DocmagApplication.groovy
@@ -0,0 +1,12 @@
+package net.deckerego.docmag
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DocmagApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run DocmagApplication, args
+	}
+}
