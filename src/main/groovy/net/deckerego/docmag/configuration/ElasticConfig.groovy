04ad7e2d0e599a1aeab3edc6084fc69bb484a108
John Ellis <deckerego@gmail.com>
2017-12-16 14:36:07 -0500

    An attempt to support Elasticsearch 6.1 - however Spring Data doesn't quite support this yet, so it will be reverted.

--- src/main/groovy/net/deckerego/docmag/configuration/ElasticConfig.groovy
+++ src/main/groovy/net/deckerego/docmag/configuration/ElasticConfig.groovy
@@ -2,7 +2,7 @@ package net.deckerego.docmag.configuration
 
 import org.elasticsearch.client.Client
 import org.elasticsearch.common.settings.Settings
-import org.elasticsearch.common.transport.InetSocketTransportAddress
+import org.elasticsearch.common.transport.TransportAddress
 import org.elasticsearch.transport.client.PreBuiltTransportClient
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -25,7 +25,7 @@ class ElasticConfig {
                 .put("cluster.name", cluster)
                 .build()
         Client client = new PreBuiltTransportClient(settings)
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port))
+                .addTransportAddress(new TransportAddress(InetAddress.getByName(host), port))
         new ElasticsearchTemplate(client)
     }
 }
